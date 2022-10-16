ace4fa1b925c84ad2dceddf5c18814dfb4dcf6fc
McDodgerman <59996045+McDodgerman@users.noreply.github.com>
2022-10-16 16:59:46 -0500

    Initial commit

---
+++ app/src/test/java/com/stargrazer/affirmations/AffirmationsAdapterTests.kt
@@ -0,0 +1,23 @@
+package com.stargrazer.affirmations
+
+import android.content.Context
+import com.stargrazer.affirmations.adapter.ItemAdapter
+import com.stargrazer.affirmations.model.Affirmation
+import org.junit.Assert.assertEquals
+import org.junit.Test
+//Gradle outdated, sorry...
+//import org.mockito.Mockito.mock
+
+class AffirmationsAdapterTests {
+    private val context = mock(Context::class.java)
+
+    @Test
+    fun adapter_size() {
+        val data = listOf(
+            Affirmation(R.string.affirmation1, R.drawable.image1),
+            Affirmation(R.string.affirmation2, R.drawable.image2)
+        )
+        val adapter = ItemAdapter(context,data)
+        assertEquals("ItemAdapter is not the correct size", data.size, adapter.itemCount)
+    }
+}
